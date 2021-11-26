6c9c7873155fbfacad3dee4fd3cdbb84697c84dd
LeonardSSH <leonardssh22@gmail.com>
2021-11-27 01:55:06 +0200

    add types

---
+++ src/client/@types/index.d.ts
@@ -0,0 +1,9 @@
+declare global {
+	interface PlayerMp {
+		customProperty: number;
+
+		customMethod(): void;
+	}
+}
+
+export {};
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,14 @@ import { SHARED_CONSTANTS } from '@shared/constants';
 
 mp.events.add('playerReady', (player) => {
 	console.log(`${player.name} is ready!`);
+
+	player.customProperty = 1;
+
+	player.customMethod = () => {
+		console.log('customMethod called.');
+	};
+
+	player.customMethod();
 });
 
 console.log(SHARED_CONSTANTS.HELLO_WORLD);
